c1241e979c6cad53356100056508ae7fae708e0a
Vinicius Ruch <viniciusruch@outlook.com>
2024-11-07 15:23:02 -0300

    commit add base

---
+++ config/api_db.sql
@@ -0,0 +1,26 @@
+CREATE DATABASE api_db;
+
+USE api_db;
+
+CREATE TABLE ferramenta(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    tipo varchar(100) not null,
+    nome varchar(100) not null,
+    descricao varchar(100) not null
+);
+
+CREATE TABLE material(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    tipo varchar(100) not null,
+    quantidade INT not null,
+    descricao varchar(100) not null,
+    preco float not null 
+);
+CREATE TABLE madeira(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    tipo varchar(100) not null,
+    quantidade INT not null,
+    descricao varchar(100) not null,
+    preco float not null ,
+    dimensoes varchar(100) not null
+);
